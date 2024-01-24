3e237c6d31e28af0c989e5f0d2cd288a5e1699f2
oliullah05 <mdolihasan@gmail.com>
2024-01-24 22:28:32 +0600

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@material-tailwind/react"
 import { Toaster } from "react-hot-toast"
+import { NavbarSimple } from "./components/Navbar/Navbar"
 
 
 function App() {
@@ -8,10 +9,7 @@ function App() {
   return (
     <>
      <Toaster/>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <Button placeholder={"djlksdns"}>button </Button>
-    </h1>
+      <NavbarSimple></NavbarSimple>
     </>
   )
 }
