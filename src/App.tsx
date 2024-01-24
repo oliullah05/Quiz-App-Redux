d2784c2d4b6b0717c3464044d7ce95913581e07b
oliullah05 <mdolihasan@gmail.com>
2024-01-24 21:32:01 +0600

    initial redux tailwind material tailwind done

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@material-tailwind/react"
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button placeholder={"djlksdns"}>button </Button>
+    </h1>
+    </>
+  )
+}
+
+export default App
